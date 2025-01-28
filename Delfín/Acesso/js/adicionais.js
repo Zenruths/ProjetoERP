b4f32adeba992d79a2188134b3e46718cd96f4a3
Zenruths <rafaelzenruths@gmail.com>
2025-01-28 18:00:18 -0300

    commit

--- Delfín/Acesso/js/adicionais.js
+++ Delfín/Acesso/js/adicionais.js
@@ -1,17 +1,13 @@
-/* Botão Ocultar/Exibir Legendas de Status na tela de Contas a Receber */
-
+// Botão Ocultar/Exibir Legendas de Status na tela de Contas a Receber
 document.getElementById('toggleButton').addEventListener('click', function () {
     var legendContent = document.getElementById('legendContent');
-    var legendContainer = document.getElementById('legendContainer');
 
     // Verifica se a legenda está visível ou não
     if (legendContent.style.display === "" || legendContent.style.display === "none") {
         legendContent.style.display = "block"; // Exibe a legenda
-        legendContainer.querySelector('ul').style.display = "block"; // Exibe a lista
         this.textContent = "Ocultar Legenda"; // Muda o texto do botão
     } else {
         legendContent.style.display = "none"; // Esconde a legenda
-        legendContainer.querySelector('ul').style.display = "none"; // Esconde a lista
         this.textContent = "Exibir Legenda"; // Muda o texto do botão
     }
 });
